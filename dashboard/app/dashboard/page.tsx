66be469e98c9cc8e2a67a42f1c2c0bb17611545e
Daniel Gorrie <dgorrie@Daniels-MacBook-Air.local>
2024-10-17 23:15:54 -0400

    add buncha shit

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -1,21 +1,22 @@
-import { InfoIcon } from "lucide-react";
-import withAuth, { AuthedPageProps } from "@hoc/with-server-auth";
+import withAuth from "@hoc/with-server-auth";
 
-const DashboardPage = ({ user }: AuthedPageProps) => {
+import FacilitiesList from "@/components/facilities-list";
+import PartnerFacilitiesList from "@/components/partner-facilities-list";
+import PartnersList from "@/components/partners-list";
+import StoreViewer from "@/components/store-viewer";
+import WasteTypesList from "@/components/waste-types-list";
+
+const DashboardPage = () => {
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
+    <div className="flex-1 w-full flex flex-row gap-12">
+      <div className="flex-1 flex flex-col gap-12">
+        <WasteTypesList />
+        <FacilitiesList />
+        <PartnerFacilitiesList />
+        <PartnersList />
       </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-64 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
+      <div className="flex-1 flex flex-col gap-12">
+        <StoreViewer />
       </div>
     </div>
   );
--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import StoreProvider from "@/lib/store/provider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -21,32 +22,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>OURO</Link>
+    <StoreProvider>
+      <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+        <body className="bg-background text-foreground">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="min-h-screen flex flex-col items-center">
+              <div className="flex-1 w-full flex flex-col gap-20 items-center">
+                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                  <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                    <div className="flex gap-5 items-center font-semibold">
+                      <Link href={"/"}>OURO</Link>
+                    </div>
+                    <ThemeSwitcher />
+                    <HeaderAuth />
                   </div>
-                  <ThemeSwitcher />
-                  <HeaderAuth />
+                </nav>
+                <div className="flex flex-col gap-20 max-w-5xl p-5">
+                  {children}
                 </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
               </div>
-            </div>
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
+            </main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
