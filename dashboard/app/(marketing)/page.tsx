bd0e626c37f11ae9e42f27ff27602707d4d5f329
lnkn <linkthegeek@outlook.com>
2025-01-14 17:09:17 -0800

    Add homepage button

--- dashboard/app/(marketing)/page.tsx
+++ dashboard/app/(marketing)/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Index = () => {
-  return <>{"OUROOOOOOO"}</>;
+  return <>
+  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+   <div> {"Welcome to Ouro Dashboard"}</div>
+    <div><Button><Link href="/dashboard"> Go to Dashboard</Link></Button></div>
+  </div>
+  </>;
 };
 
 export default Index;
