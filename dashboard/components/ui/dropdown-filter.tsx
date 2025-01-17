151dfc1c2b9b7527a3794b5c77abdc07344dff65
lnkn <linkthegeek@outlook.com>
2025-01-17 14:22:59 -0800

    add provisional datepicker, not entirely sure that this works yet. need more dates i think.

--- dashboard/components/ui/dropdown-filter.tsx
+++ dashboard/components/ui/dropdown-filter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useAppSelector, useAppDispatch } from "@hooks/store-hooks";
 import { RootState } from "@store/configuration";
 import {
@@ -14,10 +14,15 @@ import {
   toggleWasteType,
 } from "@slices/selected-waste-types-slice";
 import { Triangle, Square, Circle, House, Recycle, DiamondsFour, CaretDown } from "phosphor-react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { setStart, setEnd } from "@slices/selected-date-slice";
+import { useWasteRates } from "@hooks/use-waste-rates";
 
 
 const DropdownFilter: React.FC = () => {
   const dispatch = useAppDispatch();
+  const { data: wasteRates } = useWasteRates({ filters: ["date"] });
 
   useEffect(() => {
     dispatch(fetchPartnersIfEmpty());
@@ -28,6 +33,20 @@ const DropdownFilter: React.FC = () => {
   const partners = useAppSelector((state: RootState) => state.selectedPartners);
   const facilities = useAppSelector((state: RootState) => state.selectedFacilities);
   const wasteTypes = useAppSelector((state: RootState) => state.selectedWasteTypes);
+  const dateRange = useAppSelector((state: RootState) => state.selectedDate);
+
+  const validDates = useMemo(() => {
+    return wasteRates.map((rate) => new Date(rate.timerange));
+  }, [wasteRates]);
+
+  useEffect(() => {
+    console.log('Valid Dates:', validDates);
+    if (validDates.length > 0) {
+      const sortedDates = [...validDates].sort((a, b) => a.getTime() - b.getTime());
+      console.log('Earliest Date:', sortedDates[0]);
+      console.log('Latest Date:', sortedDates[sortedDates.length - 1]);
+    }
+  }, [validDates]);
 
   const handleToggle = (type: string, item: any) => {
     switch (type) {
@@ -43,6 +62,19 @@ const DropdownFilter: React.FC = () => {
       default:
         break;
     }
+  };
+
+  const handleDateChange = (type: string, date: Date | null) => {
+    if (!date) return;
+    if (type === 'start') {
+      dispatch(setStart(date.toISOString()));
+    } else if (type === 'end') {
+      dispatch(setEnd(date.toISOString()));
+    }
+  };
+
+  const isDateValid = (date: Date) => {
+    return validDates.some((validDate) => validDate.getTime() === date.getTime());
   };
 
   const renderDropdown = (title: string, items: any[], selectedItems: any[], type: string) => (
@@ -101,6 +133,53 @@ const DropdownFilter: React.FC = () => {
       {renderDropdown("Materials", wasteTypes.valid, wasteTypes.selected, "wasteType")}
       {renderDropdown("Facilities", facilities.valid, facilities.selected, "facility")}
       {renderDropdown("Partners", partners.valid, partners.selected, "partner")}
+      <div className="dropdown ">
+        
+          <DatePicker
+            selected={new Date(dateRange.selected.start)}
+            onChange={(date) => handleDateChange('start', date)}
+            filterDate={isDateValid}
+            customInput={
+              <button className="w-[170px] flex flex-row justify-between bg-white border-none p-2 cursor-pointer text-xs rounded-sm">
+                <div className="flex flex-row gap-2 items-end text-left">
+                  <span className="text-[10px] text-neutral-400 uppercase leading-none tracking-[.03em]">
+                    Start Date
+                  </span>
+                  <span className="text-[10px] text-neutral-400 uppercase leading-none tracking-[.03em]">
+                    {dateRange.selected.start ? new Date(dateRange.selected.start).toLocaleDateString() : "Select"}
+                  </span>
+                </div>
+                <span className="">
+                  <CaretDown color="#1C1C1C" size={12} weight="regular" />
+                </span>
+              </button>
+            }
+          />
+        
+        
+      </div>
+      <div className="dropdown ">
+        <DatePicker
+          selected={new Date(dateRange.selected.end)}
+          onChange={(date) => handleDateChange('end', date)}
+          filterDate={isDateValid}
+          customInput={
+            <button className="w-[170px] flex flex-row justify-between bg-white border-none p-2 cursor-pointer text-xs rounded-sm w-full">
+              <div className="flex flex-row gap-2 items-end text-left">
+                <span className="text-[10px] text-neutral-400 uppercase leading-none tracking-[.03em]">
+                  End Date
+                </span>
+                <span className="text-[10px] text-neutral-400 uppercase leading-none tracking-[.03em]">
+                  {dateRange.selected.end ? new Date(dateRange.selected.end).toLocaleDateString() : "Select"}
+                </span>
+              </div>
+              <span className="">
+                <CaretDown color="#1C1C1C" size={12} weight="regular" />
+              </span>
+            </button>
+          }
+        />
+      </div>
     </div>
   );
 };
