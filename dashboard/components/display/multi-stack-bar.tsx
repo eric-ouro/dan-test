d799fed6c8131da89d942bd7a20717ffe0266dbd
lnkn <linkthegeek@outlook.com>
2025-01-24 11:10:07 -0800

    table now is sorted by quantity instead of 'percentage' as 'percentage' was the same as recycle rate.

--- dashboard/components/display/multi-stack-bar.tsx
+++ dashboard/components/display/multi-stack-bar.tsx
@@ -1,7 +1,7 @@
-"use client";
+"use client"; 
 
-import { useMemo, useState } from "react";
-import DashboardDisplayHeader from "@components/dashboard-display-header";
+import { useMemo, useState } from "react"; 
+import DashboardDisplayHeader from "@components/dashboard-display-header"; 
 import {
   EnrichedWasteRateSummaryWithRatios,
   SortConfig,
@@ -13,24 +13,26 @@ type SortKey =
   | "quantity"
   | "recycled"
   | "recyclingLossRate"
-  | "processingLossRate";
+  | "processingLossRate"; 
 
 interface MultiStackBarProps {
   summaries: EnrichedWasteRateSummaryWithRatios[];
   name?: string;
-}
+} // Defining the props interface for the MultiStackBar component.
 
 const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
+  // Defining the MultiStackBar component.
   const [sortConfig, setSortConfig] = useState<
     SortConfig<EnrichedWasteRateSummaryWithRatios, SortKey>
   >({
-    key: "percentage",
+    key: "quantity",
     direction: "descending",
-  });
+  }); // Initializing state for sorting configuration with default values.
 
   const sortedSummaries = useMemo(
     () =>
       [...summaries].sort((a, b) => {
+        // Sorting the summaries based on the sortConfig state.
         if (a[sortConfig.key] < b[sortConfig.key]) {
           return sortConfig.direction === "ascending" ? -1 : 1;
         }
@@ -40,9 +42,12 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
         return 0;
       }),
     [summaries, sortConfig],
-  );
+  ); // Memoizing the sorted summaries to optimize performance.
+
+  console.log("sortedSummaries", sortedSummaries);
 
   const requestSort = (key: SortKey) => {
+    // Function to handle sorting requests.
     let direction: SortDirection = "ascending";
     if (sortConfig.key === key && sortConfig.direction === "ascending") {
       direction = "descending";
@@ -51,6 +56,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
   };
 
   const totalQuantity = useMemo(() => {
+    // Calculating the total quantity of all summaries.
     return summaries.reduce((acc, curr) => acc + curr.quantity, 0);
   }, [summaries]);
 
@@ -60,40 +66,47 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
         ...summaries.map((item) => (item.quantity / totalQuantity) * 100),
       ),
     [summaries, totalQuantity],
-  );
+  ); // Calculating the largest footprint percentage for normalization.
 
   const getHeaderClass = (key: SortKey) => {
+    // Function to get the CSS class for the table headers based on the sort key.
     return sortConfig.key === key ? "custom-underline" : "text-neutral-400";
   };
 
   return (
     <div className="dashcomponent">
+      {/* Main container for the component */}
       <div className="flex flex-col gap-3 overflow-hidden h-full">
+        {/* Flex container for the header and table */}
         <div className="sans text-xl">
+          {/* Header section */}
           {name && <DashboardDisplayHeader headerText={name} textSize="text-lg" />}
         </div>
         <div className="overflow-x-auto">
+          {/* Table container with horizontal overflow */}
           <table className="min-w-full table-auto">
+            {/* Table element */}
             <thead className="cursor-pointer text-xs text-left ">
+              {/* Table header */}
               <tr className="h-12">
                 <th className="text-neutral-400 text-xs text-left min-w-[80px] font-normal ">
                   Plastic
                 </th>
                 <th
                   onClick={() => {
-                    requestSort("percentage");
+                    requestSort("quantity");
                   }}
-                  className={` text-left min-w-[60px] font-normal ${getHeaderClass("percentage")}`}
+                  className={` text-left min-w-[60px] font-normal ${getHeaderClass("quantity")}`}
                 >
                   Footprint
                 </th>
                 <th
-                  className={` min-w-[60px] font-normal ${getHeaderClass("percentage")}`}
+                  className={` min-w-[60px] font-normal cursor-default ${getHeaderClass("percentage")}`}
                 >
                   {" "}
                 </th>
                 <th
-                  className={`text-left font-normal ${getHeaderClass("percentage")}`}
+                  className={`text-left font-normal  cursor-default ${getHeaderClass("percentage")}`}
                 >
                   {" "}
                 </th>
@@ -124,7 +137,9 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
               </tr>
             </thead>
             <tbody>
+              {/* Table body */}
               {sortedSummaries.map((item, index) => {
+                // Mapping over sorted summaries to create table rows.
                 const minWidth = item.quantity > 0 ? "10%" : "0";
                 const footprintPercentage =
                   totalQuantity > 0 ? (item.quantity / totalQuantity) * 100 : 0;
@@ -137,6 +152,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
 
                 return (
                   <tr className="align-middle h-[44px]" key={index}>
+                    {/* Table row */}
                     <td>
                       <span className="flex items-center ">
                         <span
@@ -148,13 +164,12 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
                         {displayLabel}
                       </span>
                     </td>
-                    <td className=" max-w-[60px]">
-                      {footprintPercentage.toFixed(1)}%
-                    </td>
                     <td className="text-left max-w-[60px]">
                       {item.quantity.toFixed(1).padStart(4, "0")}&nbsp;Tn
                     </td>
-
+                    <td className=" text-right max-w-[60px]">
+                      {footprintPercentage.toFixed(1)}%
+                    </td>
                     <td className="w-[100%] px-[20px]">
                       <div
                         className="h-[34px]  text-left overflow-hidden rounded-sm  flex ]"
@@ -172,8 +187,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
                           }}
                         >
                           <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-3 w-3 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="23 4 23 10 17 10" /><polyline points="1 20 1 14 7 14" /><path d="M3.51 9a9 9 0 0114.38-3.36L23 10M1 14l5.09 5.09A9 9 0 0020.49 15" /></svg> */}
-                            {/* <span>{recycleRate.toFixed(1)}%</span> */}
+                            {/* Placeholder for recycle rate */}
                           </div>
                         </div>
                         <div
@@ -184,8 +198,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
                           }}
                         >
                           <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-3 w-3 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6" /><path d="M19 6l-2 14H7L5 6" /><path d="M10 11v6" /><path d="M14 11v6" /><path d="M1 6h22" /><path d="M8 6V4a2 2 0 012-2h4a2 2 0 012 2v2" /></svg> */}
-                            {/* <span>{recyclingLossRate.toFixed(1)}%</span> */}
+                            {/* Placeholder for recycling loss rate */}
                           </div>
                         </div>
                         <div
@@ -196,8 +209,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
                           }}
                         >
                           <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-3 w-3 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6" /><path d="M19 6l-2 14H7L5 6" /><path d="M10 11v6" /><path d="M14 11v6" /><path d="M1 6h22" /><path d="M8 6V4a2 2 0 012-2h4a2 2 0 012 2v2" /></svg> */}
-                            {/* <span>{processingLossRate.toFixed(1)}%</span> */}
+                            {/* Placeholder for processing loss rate */}
                           </div>
                         </div>
                       </div>
@@ -222,4 +234,4 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
   );
 };
 
-export default MultiStackBar;
+export default MultiStackBar; // Exporting the MultiStackBar component as the default export.
