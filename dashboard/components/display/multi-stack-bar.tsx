5fcc7d83316788bd135fdd5f0dfab151524ff531
lnkn <linkthegeek@outlook.com>
2025-01-27 16:13:50 -0800

    add multistackbar variant with toggle

--- dashboard/components/display/multi-stack-bar.tsx
+++ dashboard/components/display/multi-stack-bar.tsx
@@ -13,7 +13,8 @@ type SortKey =
   | "accounted"
   | "recycled"
   | "recyclingLossRate"
-  | "processingLossRate"; 
+  | "processingLossRate"
+  | "quantity"; 
 
 interface MultiStackBarProps {
   summaries: EnrichedWasteRateSummaryWithRatios[];
@@ -98,7 +99,7 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
                   }}
                   className={` min-w-[60px] font-normal  border-foreground/20 ${getHeaderClass("accounted")} `}
                 >
-                  Footprint
+                  Accounted
                 </th>
                 <th
                   className={` min-w-[60px] cursor-default font-normal border-foreground/20 ${getHeaderClass("percentage")} `}
@@ -241,4 +242,183 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
   );
 };
 
-export default MultiStackBar; // Exporting the MultiStackBar component as the default export.
+// Define a new component variant
+const MultiStackBarVariant = ({ name, summaries }: MultiStackBarProps) => {
+  const [sortConfig, setSortConfig] = useState<
+    SortConfig<EnrichedWasteRateSummaryWithRatios, SortKey>
+  >({
+    key: "quantity",
+    direction: "descending",
+  });
+
+  const sortedSummaries = useMemo(
+    () =>
+      [...summaries].sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      }),
+    [summaries, sortConfig],
+  );
+
+  const totalQuantity = useMemo(() => {
+    return summaries.reduce((acc, curr) => acc + curr.quantity, 0);
+  }, [summaries]);
+
+  const getHeaderClass = (key: SortKey) => {
+    return sortConfig.key === key ? "custom-underline" : "text-neutral-400";
+  };
+
+  const requestSort = (key: SortKey) => {
+    let direction: SortDirection = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return (
+    <div className="dashcomponent">
+      <div className="flex flex-col ga overflow-hidden h-full">
+        <div className="sans text-xl">
+          {name && <DashboardDisplayHeader headerText={name} textSize="text-lg" />}
+        </div>
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-auto border-collapse">
+            <thead className="cursor-pointer text-xs text-left uppercase ">
+              <tr className="h-12 border-b border-foreground/20 font-normal">
+                <th className="text-neutral-400 font-normal min-w-[80px] border-foreground/20 ">
+                  Plastic
+                </th>
+                <th
+                  onClick={() => {
+                    requestSort("quantity");
+                  }}
+                  className={` min-w-[80px] font-normal  border-foreground/20 ${getHeaderClass("quantity")} `}
+                >
+                  Quantity
+                </th>
+                <th
+                  onClick={() => {
+                    requestSort("accounted");
+                  }}
+                  className={` min-w-[60px] font-normal  border-foreground/20 ${getHeaderClass("accounted")} `}
+                >
+                  Accounted
+                </th>
+                <th className="cursor-default">
+                  
+                </th>
+                <th
+                  onClick={() => {
+                    requestSort("percentage");
+                  }}
+                  className={` min-w-[60px] text-right font-normal  border-foreground/20 ${getHeaderClass("percentage")} `}
+                >
+                  Accounted %
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedSummaries.map((item, index) => {
+                const accountedPercentage =
+                  totalQuantity > 0 ? (item.accounted / totalQuantity) * 100 : 0;
+
+                return (
+                  <tr
+                    className={`align-middle h-[34px] ${
+                      index === sortedSummaries.length - 1
+                        ? ""
+                        : "border-b border-foreground/20"
+                    }`}
+                    key={index}
+                  >
+                    <td className="border-foreground/20 ">
+                      <span className="flex items-center ">
+                        <span
+                          className={`inline-block w-3 h-3 rounded-full mr-2`}
+                          style={{
+                            background: `#${item.label.display_color}`,
+                          }}
+                        ></span>
+                        {item.label.name}
+                      </span>
+                    </td>
+                    <td className="text-left border-foreground/20 ">
+                      {item.quantity.toFixed(1).padStart(4, "0")}&nbsp;Tn
+                    </td>
+                    <td className="text-left border-foreground/20 ">
+                      {item.accounted.toFixed(1).padStart(4, "0")}&nbsp;Tn
+                    </td>
+                    <td className="w-[100%] px-[20px] border-foreground/20  ">
+                      <div
+                        className="h-[17px] text-left overflow-hidden rounded-sm flex bg-foreground"
+                        style={{
+                          width: `${(item.quantity / Math.max(...sortedSummaries.map(s => s.quantity))) * 100}%`,
+                          minWidth: "10%",
+                        }}
+                      >
+                        <div
+                          className={`h-full flex items-center justify-start `}
+                          style={{
+                            width: `${accountedPercentage}%`,
+                            minWidth: "10%",
+                            background: `#${item.label.display_color}`, 
+                          }}
+                        >
+                        </div>
+                      </div>
+                    </td>
+                    <td className="text-right border-foreground/20 ">
+                      {accountedPercentage.toFixed(1)}%
+                    </td>
+                  </tr>
+                  
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Add a toggle button to switch between variants
+const MultiStackBarWithToggle = ({ name, summaries }: MultiStackBarProps) => {
+  const [showVariant, setShowVariant] = useState(false);
+
+  return (
+    <div>
+      <div className="flex items-center bg-gray-200 rounded-md p-1 text-xs tracking-tight w-fit"
+      onClick={() => setShowVariant(!showVariant)}
+      >
+        <button
+          className={`px-4 py-2 rounded-md transition-colors duration-300 ${
+            !showVariant ? "bg-white text-black" : "text-gray-500"
+          }`}
+        >
+          Tracked
+        </button>
+        <button
+          className={`px-4 py-2 rounded-md transition-colors duration-300 ${
+            showVariant ? "bg-white text-black" : "text-gray-500"
+          }`}
+        >
+          All
+        </button>
+      </div>
+      {showVariant ? (
+        <MultiStackBarVariant name={name} summaries={summaries} />
+      ) : (
+        <MultiStackBar name={name} summaries={summaries} />
+      )}
+    </div>
+  );
+};
+
+export default MultiStackBarWithToggle; // Exporting the MultiStackBar component as the default export.
