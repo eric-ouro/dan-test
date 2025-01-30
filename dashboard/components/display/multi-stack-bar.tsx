7627bedb6c5e380c9e99c2fffa54b7a52bd177bf
lnkn <linkthegeek@outlook.com>
2025-01-30 14:11:24 -0800

    swap table over to css grid. impliment amazing spacing and consistency!

--- dashboard/components/display/multi-stack-bar.tsx
+++ dashboard/components/display/multi-stack-bar.tsx
@@ -81,167 +81,52 @@ const MultiStackBar = ({ name, summaries }: MultiStackBarProps) => {
   return (
     <div className="dashcomponent">
       {/* Main container for the component */}
-      <div className="flex flex-col ga overflow-hidden h-full">
+      <div className="flex flex-col gap-2 overflow-hidden h-full">
         {/* Flex container for the header and table */}
         <div className="sans text-xl">
           {/* Header section */}
           {name && <DashboardDisplayHeader headerText={name} textSize="text-lg" />}
         </div>
         <div className="overflow-x-auto">
-          {/* Table container with horizontal overflow */}
-          <table className="min-w-full table-auto border-collapse">
-            {/* Table element */}
-            <thead className="cursor-pointer text-xs text-left uppercase ">
-              {/* Table header */}
-              <tr className="h-12 border-b border-foreground/20 ">
-                <th
-                  className="min-w-[100px] border-foreground/20 text-foreground/50"
-                >
-                  Material
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("accounted");
-                  }}
-                  className={`min-w-[60px] border-foreground/20 ${getHeaderClass("accounted")}`}
-                >
-                  Accounted
-                </th>
-                <th
-                  className={` cursor-default border-foreground/20 ${getHeaderClass("percentage")} `}
-                >
-                  {" "}
-                </th>
-                <th
-                  className={`cursor-default border-foreground/20 ${getHeaderClass("percentage")} `}
-                >
-                  {" "}
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("recycled");
-                  }}
-                  className={`text-left min-w-[80px] border-foreground/20 ${getHeaderClass("recycled")}`}
-                >
-                  Recycled
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("recyclingLossRate");
-                  }}
-                  className={`min-w-[60px] text-left border-foreground/20 ${getHeaderClass("recyclingLossRate")}`}
-                >
-                  R&nbsp;Loss
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("processingLossRate");
-                  }}
-                  className={`min-w-[60px] text-right ${getHeaderClass("processingLossRate")}`}
-                >
-                  P&nbsp;Loss
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Table body */}
-              {sortedSummaries.map((item, index) => {
-                // Mapping over sorted summaries to create table rows.
-                const minWidth = item.accounted > 0 ? "10%" : "0";
-                const footprintPercentage =
-                  totalAccounted > 0 ? (item.accounted / totalAccounted) * 100 : 0;
-                const normalizedWidth =
-                  (footprintPercentage / largestFootprintPercentage) * 100;
-                const displayLabel =
-                  item.label.name === "MixedPlastic"
-                    ? "Mixed"
-                    : item.label.name;
+          <div className="grid grid-cols-[minmax(100px,auto)_minmax(80px,auto)_1fr_minmax(80px,auto)_minmax(80px,auto)] gap-2 text-xs text-left uppercase mb-2">
+            <div className="text-foreground/50">Material</div>
+            <div onClick={() => requestSort("accounted")} className={` ${getHeaderClass("accounted")}`}>Accounted</div>
+            <div onClick={() => requestSort("recycled")} className={`text-right ${getHeaderClass("recycled")}`}>Recycled</div>
+            <div onClick={() => requestSort("recyclingLossRate")} className={`text-right  ${getHeaderClass("recyclingLossRate")}`}>R&nbsp;Loss</div>
+            <div onClick={() => requestSort("processingLossRate")} className={`text-right ${getHeaderClass("processingLossRate")}`}>P&nbsp;Loss</div>
+          </div>
+          <div>
+            {sortedSummaries.map((item, index) => {
+              const minWidth = item.accounted > 0 ? "10%" : "0";
+              const footprintPercentage = totalAccounted > 0 ? (item.accounted / totalAccounted) * 100 : 0;
+              const normalizedWidth = (footprintPercentage / largestFootprintPercentage) * 100;
+              const displayLabel = item.label.name === "MixedPlastic" ? "Mixed" : item.label.name;
 
-                return (
-                  <tr
-                    className={`align-middle h-[39px] ${
-                      index === sortedSummaries.length - 1
-                        ? ""
-                        : "border-b border-foreground/20"
-                    }`}
-                    key={index}
-                  >
-                    {/* Table row */}
-                    <td className="border-foreground/20 text-foreground">
-                      <span className="flex items-center ">
-                        <span
-                          className="inline-block w-3 h-3 rounded-full mr-2"
-                          style={{
-                            background: `#${item.label.display_color}`,
-                          }}
-                        ></span>
-                        {displayLabel}
-                      </span>
-                    </td>
-                    <td className={`text-left border-foreground/20 ${getHeaderClass("accounted")}`}>
-                      {item.accounted.toFixed(1).padStart(4, "0")}t
-                    </td>
-                    <td className={`text-left border-foreground/20 ${getHeaderClass("accounted")}`}>
-                      {footprintPercentage.toFixed(1)}%
-                    </td>
-                    <td className="w-[100%]  border-foreground/20 ">
-                      <div
-                        className="h-[17.34px] px-[17.34px] text-left overflow-hidden  flex"
-                        style={{
-                          width: `${Math.max(normalizedWidth, 10)}%`,
-                          minWidth,
-                        }}
-                      >
-                        <div
-                          className={`h-full flex items-center justify-start`}
-                          style={{
-                            width: `${Math.max(item.recycleRate, 10)}%`,
-                            minWidth,
-                            background: `#${item.label.display_color}`,
-                          }}
-                        >
-                          <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* Placeholder for recycle rate */}
-                          </div>
-                        </div>
-                        <div
-                          className={`h-full flex items-center justify-start bg-neutral-400`}
-                          style={{
-                            width: `${Math.max(item.recyclingLossRate, 10)}%`,
-                            minWidth,
-                          }}
-                        >
-                          <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* Placeholder for recycling loss rate */}
-                          </div>
-                        </div>
-                        <div
-                          className={`h-full flex items-center justify-start bg-neutral-500`}
-                          style={{
-                            width: `${Math.max(item.processingLossRate, 10)}%`,
-                            minWidth,
-                          }}
-                        >
-                          <div className="text-white text-xs ml-1 opacity-60 flex items-center">
-                            {/* Placeholder for processing loss rate */}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className={`text-left border-foreground/20 ${getHeaderClass("recycled")}`}>
-                      {item.recycleRate.toFixed(1)}%
-                    </td>
-                    <td className={`text-left border-foreground/20 ${getHeaderClass("recyclingLossRate")}`}>
-                      {item.recyclingLossRate.toFixed(1)}%
-                    </td>
-                    <td className={`text-right border-foreground/20 ${getHeaderClass("processingLossRate")}`}>
-                      {item.processingLossRate.toFixed(1)}%
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+              return (
+                <div className="grid grid-cols-[minmax(100px,auto)_minmax(80px,auto)_minmax(80px,auto)_1fr_minmax(80px,auto)_minmax(80px,auto)_minmax(80px,auto)] gap-2 align-middle py-2 border-t border-foreground/20" key={index}>
+                  <div className="text-foreground flex-none">
+                    <span className="flex items-center">
+                      <span className="inline-block w-[17.34px] h-[17.34px] flex-shrink-0 rounded-full mr-2" style={{ background: `#${item.label.display_color}` }}></span>
+                      {displayLabel}
+                    </span>
+                  </div>
+                  <div className={`text-left ${getHeaderClass("accounted")} flex-none`}>{item.accounted.toFixed(1).padStart(4, "0")}t</div>
+                  <div className={`text-left ${getHeaderClass("accounted")} flex-none`}>{footprintPercentage.toFixed(1)}%</div>
+                  <div className="w-full flex items-center">
+                    {/* Recycling bar */}
+                    <div className="h-[17.34px] text-left overflow-hidden flex items-center" style={{ width: `${Math.max(normalizedWidth, 10)}%` }}>
+                      <div className={`h-full flex items-center justify-start`} style={{ width: `${Math.max(item.recycleRate, 10)}%`, background: `#${item.label.display_color}` }}></div>
+                      <div className={`h-full flex items-center justify-start bg-neutral-400`} style={{ width: `${Math.max(item.recyclingLossRate, 10)}%` }}></div>
+                      <div className={`h-full flex items-center justify-start bg-neutral-500`} style={{ width: `${Math.max(item.processingLossRate, 10)}%` }}></div>
+                    </div>
+                  </div>
+                  <div className={`text-right ${getHeaderClass("recycled")} flex-none`}>{item.recycleRate.toFixed(1)}%</div>
+                  <div className={`text-right ${getHeaderClass("recyclingLossRate")} flex-none`}>{item.recyclingLossRate.toFixed(1)}%</div>
+                  <div className={`text-right ${getHeaderClass("processingLossRate")} flex-none`}>{item.processingLossRate.toFixed(1)}%</div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
@@ -289,105 +174,48 @@ const MultiStackBarVariant = ({ name, summaries }: MultiStackBarProps) => {
 
   return (
     <div className="dashcomponent">
-      <div className="flex flex-col ga overflow-hidden h-full">
+      <div className="flex flex-col overflow-hidden h-full gap-2">
         <div className="sans text-xl">
           {name && <DashboardDisplayHeader headerText={name} textSize="text-lg" />}
         </div>
         <div className="overflow-x-auto">
-          <table className="min-w-full table-auto border-collapse">
-            <thead className="cursor-pointer text-xs text-left uppercase ">
-              <tr className="h-12 border-b border-foreground/20  ">
-                <th className="text-neutral-400 min-w-[100px] border-foreground/20">
-                  Material
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("quantity");
-                  }}
-                  className={` min-w-[80px] border-foreground/20 ${getHeaderClass("quantity")} `}
-                >
-                  Quantity
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("accounted");
-                  }}
-                  className={` min-w-[60px] border-foreground/20 ${getHeaderClass("accounted")} `}
-                >
-                  Accounted
-                </th>
-                <th className="cursor-default">
-                  
-                </th>
-                <th
-                  onClick={() => {
-                    requestSort("percentage");
-                  }}
-                  className={` min-w-[60px] text-right border-foreground/20 ${getHeaderClass("percentage")} `}
-                >
-                  Accounted&nbsp;%
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {sortedSummaries.map((item, index) => {
-                const accountedPercentage =
-                  totalQuantity > 0 ? (item.accounted / totalQuantity) * 100 : 0;
+          <div className="grid grid-cols-[minmax(100px,auto)_minmax(80px,auto)_minmax(80px,auto)_1fr_minmax(80px,auto)] gap-2 text-xs text-left uppercase mb-2">
+            <div className="text-foreground/50">Material</div>
+            <div onClick={() => requestSort("quantity")} className={`${getHeaderClass("quantity")}`}>Quantity</div>
+            <div onClick={() => requestSort("accounted")} className={`${getHeaderClass("accounted")}`}>Accounted</div>
+            <div className="cursor-default"></div>
+            <div onClick={() => requestSort("percentage")} className={`text-right ${getHeaderClass("percentage")}`}>Accounted %</div>
+          </div>
+          <div>
+            {sortedSummaries.map((item, index) => {
+              const accountedPercentage = totalQuantity > 0 ? (item.accounted / totalQuantity) * 100 : 0;
 
-                return (
-                  <tr
-                    className={`align-middle h-[39px] ${
-                      index === sortedSummaries.length - 1
-                        ? ""
-                        : "border-b border-foreground/20"
-                    }`}
-                    key={index}
-                  >
-                    <td className="border-foreground/20 ">
-                      <span className="flex items-center ">
-                        <span
-                          className={`inline-block w-3 h-3 rounded-full mr-2`}
-                          style={{
-                            background: `#${item.label.display_color}`,
-                          }}
-                        ></span>
-                        {item.label.name}
-                      </span>
-                    </td>
-                    <td className="text-left border-foreground/20 ">
-                      {item.quantity.toFixed(1).padStart(4, "0")}t
-                    </td>
-                    <td className="text-left border-foreground/20 ">
-                      {item.accounted.toFixed(1).padStart(4, "0")}t
-                    </td>
-                    <td className="w-[100%]  border-foreground/20  ">
-                      <div
-                        className="h-[17.34px] text-left overflow-hidden  flex bg-foreground"
-                        style={{
-                          width: `${(item.quantity / Math.max(...sortedSummaries.map(s => s.quantity))) * 100}%`,
-                          minWidth: "10%",
-                        }}
-                      >
-                        <div
-                          className={`h-full flex items-center justify-start `}
-                          style={{
-                            width: `${accountedPercentage}%`,
-                            minWidth: "10%",
-                            background: `#${item.label.display_color}`, 
-                          }}
-                        >
-                        </div>
-                      </div>
-                    </td>
-                    <td className="text-right border-foreground/20 ">
-                      {accountedPercentage.toFixed(1)}%
-                    </td> 
-                  </tr>
-                  
-                );
-              })}
-            </tbody>
-          </table>
+              return (
+                <div className="grid grid-cols-[minmax(100px,auto)_minmax(80px,auto)_minmax(80px,auto)_1fr_minmax(80px,auto)] gap-2 align-middle py-2 border-t border-foreground/20" key={index}>
+                  <div className="text-foreground">
+                    <span className="flex items-center ">
+                      <span className={`inline-block w-[17.34px] h-[17.34px] flex-shrink-0 rounded-full mr-2`} style={{ background: `#${item.label.display_color}` }}></span>
+                      {item.label.name}
+                    </span>
+                  </div>
+                  <div className="text-left">
+                    {item.quantity.toFixed(1).padStart(4, "0")}t
+                  </div>
+                  <div className="text-left">
+                    {item.accounted.toFixed(1).padStart(4, "0")}t
+                  </div>
+                  <div className="w-[100%] ">
+                    <div className="h-[17.34px] text-left overflow-hidden flex bg-foreground/50" style={{ width: `${(item.quantity / Math.max(...sortedSummaries.map(s => s.quantity))) * 100}%` }}>
+                      <div className={`h-full flex items-center justify-start`} style={{ width: `${accountedPercentage}%`, background: `#${item.label.display_color}` }}></div>
+                    </div>
+                  </div>
+                  <div className="text-right">
+                    {accountedPercentage.toFixed(1)}%
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
