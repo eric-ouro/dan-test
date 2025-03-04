d2193f6dbeb2da7ae71204c26626bb176663d29f
lnkn <linkthegeek@outlook.com>
2025-03-04 09:17:26 -0800

    remove signin/signup buttons from header auth

--- dashboard/components/header-auth.tsx
+++ dashboard/components/header-auth.tsx
@@ -41,15 +41,6 @@ export default async function AuthButton() {
         </div>
       </div>
     </div>
-  ) : (
-    <div className="flex gap-2 t">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
-    </div>
-  );
+  ) : null;
 }
 
