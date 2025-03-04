fc646f527314e85b1bdeb5b2cc781a9220ab8eae
lnkn <linkthegeek@outlook.com>
2025-03-03 16:03:27 -0800

    add auto redirect to /dashboard so demoing isnt bad.

--- dashboard/app/(marketing)/page.tsx
+++ dashboard/app/(marketing)/page.tsx
@@ -1,11 +1,22 @@
+'use client';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to /dashboard when the component mounts
+    router.push("/dashboard");
+  }, [router]);
+
   return <>
   <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
    <div> {"Welcome to Ouro Dashboard"}</div>
     <div><Button><Link href="/dashboard"> Go to Dashboard</Link></Button></div>
+    
   </div>
   </>;
 };
