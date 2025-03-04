72c29f6bcebc1c4953da5372e5c17b9731bb0467
lnkn <linkthegeek@outlook.com>
2025-03-04 08:05:01 -0800

    revert redirect change so it doesnt break

--- dashboard/app/(marketing)/page.tsx
+++ dashboard/app/(marketing)/page.tsx
@@ -1,17 +1,7 @@
-'use client';
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Index = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to /dashboard when the component mounts
-    router.push("/dashboard");
-  }, [router]);
-
   return <>
   <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
    <div> {"Welcome to Ouro Dashboard"}</div>
